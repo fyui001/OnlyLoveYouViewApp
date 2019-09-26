a550c65a2f3fd57694a05574277a420e7870b0bc
fyui001 <yun.cod@gmail.com>
2019-09-27 02:11:59 +0900

    init commit

---
+++ build/webpack.base.config.js
@@ -0,0 +1,73 @@
+'use strict'
+
+const webpack = require('webpack')
+const path = require('path')
+const rm = require('rimraf')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+/* delete files */
+rm.sync(path.join(__dirname, '..', 'prod'))
+rm.sync(path.join(__dirname, '..', 'dist'))
+
+module.exports = {
+    entry: {
+        app: './src/index.tsx'
+    },
+    output: {
+        path: path.join(__dirname, '..', 'dist')
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    },
+
+    module: {
+        rules: [
+            {
+                test: [/\.ts$/, /\.tsx$/],
+                include: path.resolve("src"),
+                use: [
+                    {
+                        loader: 'thread-loader',
+                        options: {
+                            workers: require('os').cpus().length - 1
+                        }
+                    },
+                    {
+                        loader: 'babel-loader'
+                    },
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            transpileOnly: true,
+                            happyPackMode: true
+                        }
+                    },
+                ]
+            },
+            {
+                test: [/\.js$/, /\.jsx$/],
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                    }
+                }
+            },
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './public/index.html',
+            inject: true
+        }),
+    ]
+}
